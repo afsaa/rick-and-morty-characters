51004bfe49c058fb9b2aeb44c8248490f2d223a7
Andrés Saam <andressaa.dev@gmail.com>
2020-09-17 10:22:14 -0500

    Agregando la funcionalidad de infinite scroll

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,6 +1,15 @@
 const BASE_URL = "https://rickandmortyapi.com/api";
 
-export async function fetchCharacters() {
+export async function fetchCharacters(nextPageUrl) {
+  if (nextPageUrl) {
+    return fetch(nextPageUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
   return fetch(`${BASE_URL}/character`)
     .then((res) => {
       return res.json();
--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -1,17 +1,43 @@
 import { fetchCharacters } from "../../api/";
 
+// DOM elements
 const rootElement = document.querySelector("#root");
 const mainHeading = document.createElement("h1");
 
-export const home = async () => {
-  rootElement.style.textAlign = "center";
-  mainHeading.innerHTML = "Rick & Morty Characters";
-  mainHeading.style.textAlign = "center";
-  rootElement.appendChild(mainHeading);
+// Info API data
+let nextPage = "";
 
-  await fetchCharacters()
-    .then(({ results }) => {
-      console.log(results);
+// Loading characters
+const loadCharacters = async (nextPageUrl) => {
+  // In case we reach the max number of characters
+  if (nextPageUrl === null) {
+    const noMoreCharactersHeading = document.createElement("h3");
+    noMoreCharactersHeading.innerHTML = "No hay más personajes!";
+    noMoreCharactersHeading.style.textAlign = "center";
+    rootElement.appendChild(noMoreCharactersHeading);
+  }
+
+  // First time loading
+  if (nextPageUrl === "") {
+    await fetchCharacters()
+      .then(({ info, results }) => {
+        nextPage = info.next;
+        results.map((character) => {
+          const characterImage = document.createElement("img");
+          characterImage.src = character.image;
+          characterImage.setAttribute("class", "img-thumbnail");
+          rootElement.appendChild(characterImage);
+        });
+      })
+      .catch((err) => {
+        console.error("Error", err);
+      });
+  }
+
+  // Loading next page characters
+  await fetchCharacters(nextPageUrl)
+    .then(({ info, results }) => {
+      nextPage = info.next;
       results.map((character) => {
         const characterImage = document.createElement("img");
         characterImage.src = character.image;
@@ -22,4 +48,23 @@ export const home = async () => {
     .catch((err) => {
       console.error("Error", err);
     });
+};
+
+export const home = () => {
+  rootElement.style.textAlign = "center";
+  mainHeading.innerHTML = "Rick & Morty Characters";
+  mainHeading.style.textAlign = "center";
+  rootElement.appendChild(mainHeading);
+
+  // Detect when scrolled to bottom.
+  rootElement.addEventListener("scroll", function () {
+    if (
+      rootElement.scrollTop + rootElement.clientHeight >=
+      rootElement.scrollHeight - 200
+    ) {
+      loadCharacters(nextPage);
+    }
+  });
+
+  loadCharacters();
 };
