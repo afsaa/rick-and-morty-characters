6b17186a5e030913c6febf73bc6d6f6e22e73761
Andrés Saam <andressaa.dev@gmail.com>
2020-09-16 19:11:34 -0500

    Estableciendo la estructura base de la app

--- src/app/pages/home/home.js
+++ src/app/pages/home/home.js
@@ -3,5 +3,6 @@ const mainHeading = document.createElement("h1");
 
 export const home = () => {
   mainHeading.innerHTML = "Rick & Morty Characters";
+  mainHeading.style.textAlign = "center";
   rootElement.appendChild(mainHeading);
 };
